c6a3ae4bd036b6138e31e373275e7df34b48c3df
MadJack1906 <cho.suhoond.74@gmail.com>
2022-03-25 01:20:40 +0800

    Added the UI in the activity_mail.xml

--- app/src/main/java/com/example/firstaiduidesign/MainActivity.kt
+++ app/src/main/java/com/example/firstaiduidesign/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.firstaiduidesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.firstaiduidesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cardAsymptomatic.setOnClickListener { Toast.makeText(it.context, "Asymptomatic", Toast.LENGTH_SHORT).show() }
     }
 }
